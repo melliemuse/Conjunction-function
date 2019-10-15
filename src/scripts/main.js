4a1be2deab2924cebd618b2713968cd857013675
Melody Stern <melliemuse@gmail.com>
2019-10-15 13:13:29 -0500

    updated javascript and html files

---
+++ src/scripts/main.js
@@ -0,0 +1,11 @@
+
+let conjoinedWord;
+console.log(conjoinedWord);
+
+const conjunction = function (firstWord, secondWord) {
+    conjoinedWord = `${firstWord} ${secondWord}`;
+    return conjoinedWord;
+}
+
+conjunction("Master", "Card")
+console.log(conjoinedWord)
